66acaa7f844b72a903dfaef96872bed3c840d122
Shobben <hobbe@kennerberg.se>
2024-09-03 16:44:27 +0200

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputHorizontal = Input.GetAxis("Horizontal");
+        float inputVertical = Input.GetAxis("Vertical");
+        Vector3 input = new Vector3(inputHorizontal, inputVertical, 0);
+        transform.position += input * 0.01f;
+    }
+}
