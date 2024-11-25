1e01e311c48e182d951365d4ce12a0607ff5203d
Shobben <hobbe@kennerberg.se>
2024-11-25 13:38:15 +0100

    Added enemy movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     private Health healthScript;
+    [SerializeField] private float movementSpeed = 1;
+
     void Start()
     {
         healthScript = GetComponent<Health>();
     }
 
     void Update()
+    {
+        // Move towards player.
+        Vector3 playerPos = GameObject.Find("Player").transform.position;
+        Vector3 movement = playerPos - transform.position;
+        movement = movement.normalized * movementSpeed;
+        transform.position += movement;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         
     }
